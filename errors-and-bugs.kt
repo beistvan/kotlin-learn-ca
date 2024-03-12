07423585b09a33dc4e2d2795972b5f27d864807f
Beregszászi István <beistvan@gmail.com>
2024-03-12 12:05:40 +0100

    Errors And Bugs

---
+++ errors-and-bugs.kt
@@ -0,0 +1,15 @@
+fun main() {
+  println("An error a day keeps the programmer away.")
+}
+
+/*
+fun main() {
+  printlnn("To err is human.")
+}
+ */
+
+/*
+Error.kt:2:3: error: unresolved reference: printlnn
+  printlnn("To err is human.")
+  ^
+*/
