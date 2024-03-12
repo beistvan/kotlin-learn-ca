58154a682df48858792aafc26368fa73656e4d0e
Beregszászi István <beistvan@gmail.com>
2024-03-12 12:04:04 +0100

    Comments

---
+++ comments.kt
@@ -0,0 +1,19 @@
+fun main() {
+  println("Howdy, partner!") 
+  //Code prints "Howdy, partner!" to the terminal.
+  /*
+  main
+  println
+  */
+}
+
+// To be or not to be: that is the question.
+
+/*
+If you prick us, do we not bleed?
+If you tickle us, do we not laugh? 
+If you poison us, do we not die? 
+And if you wrong us, shall we not revenge?
+
+- William Shakespeare, The Merchant of Venice 
+*/
