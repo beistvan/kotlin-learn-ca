5fb3b371b926a07b394cf1c2e79afccdc06893ec
Beregszászi István <beistvan@gmail.com>
2024-03-12 13:18:44 +0100

    ASCII Art

---
+++ ascii-art.kt
@@ -0,0 +1,24 @@
+// Write your code below
+/*
+     .....      
+  .         .   
+ .   0  0    .  
+  ...........   
+   .. __ ..     
+  . .|C_|. .    
+ .  ......  .   
+     .  .       
+     .  .       
+*/
+
+fun main() {
+  println("     .....")
+  println("  .         .")
+  println(" .   0  0    .")
+  println("  ...........")
+  println("   .. __ ..")
+  println("  . .|C_|. .")
+  println(" .  ......  .")
+  println("     .  .")
+  println("     .  .") 
+}
